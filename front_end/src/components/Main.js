5cc5f530efd8e2e918d49b95c32d99f27d03357a
JIYEONGSTAR <53474999+JIYEONGSTAR@users.noreply.github.com>
2021-05-05 22:22:18 +0900

    IMG_URL change

--- front_end/src/components/Main.js
+++ front_end/src/components/Main.js
@@ -5,19 +5,21 @@ import ServiceCard from "./cards/ServiceCard";
 
 function Main(props) {
   let data = [props.booklist[0], props.booklist[1], props.booklist[2]];
+  console.log(props.booklist);
   return (
     <>
       <div className="banner">
-        <img src="/bannerImg.png" alt="" />
+        <img src={process.env.PUBLIC_URL + "/bannerImg.png"} alt="" />
       </div>
+
       <BookList data={data} />
       <div className="service">
         <p>추후서비스</p>
         <div className="service__card">
-          <ServiceCard image="/future1.jpg" />
-          <ServiceCard image="/future2.jpg" />
-          <ServiceCard image="/future3.jpg" />
-          <ServiceCard image="/future4.jpg" />
+          <ServiceCard image={process.env.PUBLIC_URL + "/future1.jpg"} />
+          <ServiceCard image={process.env.PUBLIC_URL + "/future2.jpg"} />
+          <ServiceCard image={process.env.PUBLIC_URL + "/future3.jpg"} />
+          <ServiceCard image={process.env.PUBLIC_URL + "/future4.jpg"} />
         </div>
       </div>
     </>
